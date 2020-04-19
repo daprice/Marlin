f097340398b5288403d7776950e4e8a8a060601f
Dale Price <daprice@mac.com>
2020-04-18 19:30:22 -0500

    Adjust fan PWM levels

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -376,8 +376,8 @@
  *
  * Define one or both of these to override the default 0-255 range.
  */
-//#define FAN_MIN_PWM 50
-//#define FAN_MAX_PWM 128
+#define FAN_MIN_PWM 77
+#define FAN_MAX_PWM 180
 
 /**
  * FAST PWM FAN Settings
